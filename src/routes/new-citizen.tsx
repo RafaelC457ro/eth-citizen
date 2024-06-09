243d02fa0a85b4cc453624fc6532ad69b91a513b
Rafael Castro <rccastrorafael@gmail.com>
2024-06-09 17:12:13 -0300

    fix padding

--- src/routes/new-citizen.tsx
+++ src/routes/new-citizen.tsx
@@ -7,7 +7,7 @@ export function NewCitizen() {
   const { account } = useAccount()
   return (
     <main className="container mx-auto px-4 flex items-center justify-center py-8">
-      <div className="w-full lg:w-1/2">
+      <div className="w-full lg:w-1/2 space-y-8">
         <div>
           <h3 className="text-lg font-medium">New Citizen</h3>
           <p className="text-sm text-muted-foreground">
